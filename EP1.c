296aa60e855ef84b34b1a5c8d8875d26d99aa78d
André Morales <an.dre.m@outlook.com>
2024-05-17 11:42:16 -0300

    feat: início da implementação de operação aritmética ARIT

--- EP1.c
+++ EP1.c
@@ -1,6 +1,7 @@
 #include "driverEP1.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 // Definição dos registradores do processador
 typedef struct {
@@ -14,11 +15,20 @@ typedef struct {
 	uint16_t PSW;
 } Registers;
 
+typedef struct {
+	Registers* registers;
+	uint16_t* memory;
+	uint16_t memorySize;
+} Emul;
+
 void pause();
 void badInstruction(Registers* r);
 void dumpRegisters(Registers* regs);
 void verifyAddress(Registers* r, uint16_t addr, uint16_t size);
 void fault(Registers* r);
+void faultMsg(Emul* emul, const char* fmt, ...);
+void doArit(Emul* emul, uint16_t argument);
+uint16_t* getRegisterWithBits(Registers* r, uint8_t code);
 
 int processa (short int* m, int memSize) {
 	uint16_t* memory = (uint16_t*)m;
@@ -33,6 +43,12 @@ int processa (short int* m, int memSize) {
 	r.D = 0;
 	r.R = 0;
 	r.PSW = 0;
+
+	// Estrutura do emulador
+	Emul emul;
+	emul.memory = memory;
+	emul.memorySize = memSize;
+	emul.registers = &r;
 
 	do {
 		// Lê a instrução atual
@@ -81,7 +97,10 @@ int processa (short int* m, int memSize) {
 
 			break;
 
+		// ARIT(x) -- Opcode (0110)b
+		// Executa uma operação aritmética.
 		case 0x6:
+			doArit(&emul, argument);
 			break;
 
 		// Instrução desconhecida
@@ -100,6 +119,59 @@ int processa (short int* m, int memSize) {
 	printf("\nCPU Halted.\n");
 }
 
+void doArit(Emul* emul, uint16_t argument) {
+	// Extrai os 3 bits que determinam a operação aritmética a realizar
+	uint8_t bitsOpr = (argument & 0b111000000000) >> 9;
+
+	// Extrai os 3 bits que definem o registrador destino da operação
+	uint8_t bitsRes = (argument & 0b000111000000) >> 6;
+
+	// Extrai os bits que definem o registrador do primeiro operando
+	uint8_t bitsOp1 = (argument & 0b000000111000) >> 3;
+
+	// Extrai os bits que definem o registrador do segundo operando
+	uint8_t bitsOp2 =  argument & 0b000000000111;
+
+	// Obtém o registrador destino usando os bits de Res
+	uint16_t* regRes = getRegisterWithBits(emul->registers, bitsRes);
+	if (!regRes) {
+		faultMsg(emul, "Invalid arit register destination code: %i\n", bitsRes);
+		return;
+	}
+
+	// Obtém o registrador destino usando os bits de Op1
+	uint16_t* regOp1 = getRegisterWithBits(emul->registers, bitsOp1);
+	if (!regRes) {
+		faultMsg(emul, "Invalid arit register op1 code: %i\n", bitsOp1);
+		return;
+	}
+
+	// Obtém o registrador destino usando os bits de Op2
+	uint16_t* regOp2 = NULL;
+	regOp2 = getRegisterWithBits(emul->registers, bitsOp2);
+	if (!regRes) {
+		faultMsg(emul, "Invalid arit register op2 code: %i\n", bitsOp2);
+		return;
+	}
+}
+
+uint16_t* getRegisterWithBits(Registers* r, uint8_t code) {
+	switch (code) {
+	case 0x0:
+		return &r->A;
+	case 0x1:
+		return &r->B;
+	case 0x2:
+		return &r->C;
+	case 0x3:
+		return &r->D;
+	case 0x6:
+		return &r->R;
+	default:
+		return NULL;
+	}
+}
+
 void verifyAddress(Registers* r, uint16_t addr, uint16_t size) {
 	if (addr >= size) {
 		printf("[!] Memory access out of bounds (%x)\n", addr);
@@ -116,6 +188,16 @@ void fault(Registers* r) {
 	printf("\n-- Fault: Execution paused. --\n");
 	dumpRegisters(r);
 	pause();
+}
+
+void faultMsg(Emul* emul, const char* fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	printf("\n[!] Fault: ");
+	vfprintf(stdout, fmt, args);
+	dumpRegisters(emul->registers);
+	pause();
+	va_end(args);
 }
 
 void dumpRegisters(Registers* regs) {
